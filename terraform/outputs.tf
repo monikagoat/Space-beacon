ab8d54c7da6c2a38aad025375c6294e66d63864b
monikagoat <monikamkoziol@gmail.com>
2023-06-30 10:58:14 +0200

    added config to deploy helm app to the cluster

---
+++ terraform/outputs.tf
@@ -0,0 +1,23 @@
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = data.aws_eks_cluster.cluster.certificate_authority[0].data
+}
